251abd9a60d847621b19ce19e95c845e61113f00
CupricWolf <marcello.s.nicoletti@gmail.com>
2017-10-26 23:17:13 -0600

    Adding methods for solution class.

--- src/Solution.java
+++ src/Solution.java
@@ -1,4 +1,16 @@
 public class Solution {
     int total;
+    int previousTotal;
     boolean fromFront;
+
+    public Solution (int total, int previousTotal, boolean fromFront) {
+        this.total = total;
+        this.previousTotal = previousTotal;
+        this.fromFront = fromFront;
+    }
+
+    @Override
+    public String toString () {
+        return String.format("%d,%s", total, (fromFront ? "F(↓)" : "B(←)"));
+    }
 }
