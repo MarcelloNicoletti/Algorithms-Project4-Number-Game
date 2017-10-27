cb6fcb52f30343744afa7a47b165acd5318f57fe
CupricWolf <marcello.s.nicoletti@gmail.com>
2017-10-27 02:51:50 -0600

    Added method that allows user to play against the computer.

--- src/Main.java
+++ src/Main.java
@@ -21,6 +21,10 @@ public class Main {
         int bestScore = calcGameSolution(digits.length - 1, 0, digits, memo)
                 .total;
         memo.printMatrix();
+
+        System.out.println("\nPlaying the game against computer.\n");
+
+        playGame(digits, memo);
     }
 
     private static Solution calcGameSolution (int i, int j, int[] digits,
@@ -51,5 +55,69 @@ public class Main {
         }
         memo.memoize(i, j, sol);
         return sol;
+    }
+
+    private static void playGame (int[] digits, MemoMatrix<Solution> memo) {
+        int i = digits.length - 1, // Back pointer
+                j = 0, // Front pointer
+                round = 1,
+                sumPlayer = 0,
+                sumComputer = 0;
+
+        while (i >= j) {
+            System.out.printf("Round %d: ", round);
+            for (int k = 0; k < digits.length; k++) {
+                if (k < j || k > i) {
+                    System.out.print("  ");
+                } else {
+                    System.out.print(digits[k] + " ");
+                }
+            }
+            System.out.printf(" Player: %d, Computer: %d%n", sumPlayer,
+                    sumComputer);
+
+            if (round % 2 == 0) {
+                // Computer Turn
+                Solution sol = memo.recall(i, j);
+                if (sol.fromFirst) {
+                    System.out.println("Computer chooses First");
+                    sumComputer += digits[j];
+                    j++;
+                } else {
+                    System.out.println("Computer chooses Last");
+                    sumComputer += digits[i];
+                    i--;
+                }
+            } else {
+                // Player Turn
+                System.out.printf("Take first (%d) or last (%d)? > ",
+                        digits[j], digits[i]);
+                char choice = stdIn.nextLine().charAt(0);
+
+                if (choice == 'f' || choice == 'F' || choice - '0' ==
+                        digits[j]) {
+                    System.out.println("Player chooses First");
+                    sumPlayer += digits[j];
+                    j++;
+                } else {
+                    System.out.println("Player chooses Last");
+                    sumPlayer += digits[i];
+                    i--;
+                }
+            }
+
+            round++;
+        }
+
+        System.out.println();
+        if (sumPlayer > sumComputer) {
+            System.out.printf("Player wins %d to %d.", sumPlayer, sumComputer);
+        } else if (sumComputer > sumPlayer) {
+            System.out.printf("Computer wins %d to %d.", sumComputer,
+                    sumPlayer);
+        } else {
+            System.out
+                    .printf("Player and Computer tied at %d each.", sumPlayer);
+        }
     }
 }
