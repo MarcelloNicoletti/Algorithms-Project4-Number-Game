9234aef863fa16ffda99429a7bb377947ea43369
CupricWolf <marcello.s.nicoletti@gmail.com>
2017-10-27 02:52:45 -0600

    Fixed bug from not clearing the new line from the buffer

--- src/Main.java
+++ src/Main.java
@@ -6,13 +6,15 @@ public class Main {
 
     public static void main (String args[]) {
         System.out.println("Welcome to the number game.");
-        System.out.print("How many digits will there be? >");
-        int numDigits = stdIn.nextInt(); stdIn.nextLine();
+        System.out.print("How many digits will there be? > ");
+        int numDigits = stdIn.nextInt();
+        stdIn.nextLine();
         int[] digits = new int[numDigits];
 
         for (int i = 0; i < numDigits; i++) {
-            System.out.print("Enter digit for position " + (i + 1) + " >");
+            System.out.print("Enter digit for position " + (i + 1) + " > ");
             digits[i] = stdIn.nextInt() % 10;
+            stdIn.nextLine();
         }
         System.out.println("Playing with " + Arrays.toString(digits));
 
