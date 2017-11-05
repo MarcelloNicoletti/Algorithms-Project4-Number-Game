c7a2182816eae96290cbb12c69be8cea765ded6a
CupricWolf <marcello.s.nicoletti@gmail.com>
2017-11-05 11:55:12 -0700

    Modifying to allow arbitrary ints rather than only digits.

--- src/Main.java
+++ src/Main.java
@@ -7,38 +7,38 @@ public class Main {
     public static void main (String args[]) {
         System.out.println("Welcome to the number game.");
 
-        int[] digits = getDigits();
+        int[] numbers = getNumbers();
 
-        System.out.println("Playing with " + Arrays.toString(digits));
+        System.out.println("Playing with " + Arrays.toString(numbers));
 
-        MemoMatrix<GameSolution> memo = new MemoMatrix<>(digits.length,
-                digits.length);
-        int bestScore = calcGameSolutions(digits.length - 1, 0, digits, memo)
+        MemoMatrix<GameSolution> memo = new MemoMatrix<>(numbers.length,
+                numbers.length);
+        int bestScore = calcGameSolutions(numbers.length - 1, 0, numbers, memo)
                 .total;
         memo.printMatrix();
 
         System.out.println("\nPlaying the game against computer.\n");
 
-        playGame(digits, memo);
+        playGame(numbers, memo);
     }
 
-    private static int[] getDigits () {
-        System.out.print("How many digits will there be? > ");
-        int numDigits = stdIn.nextInt();
+    private static int[] getNumbers () {
+        System.out.print("How many numbers will there be? > ");
+        int numNumbers = stdIn.nextInt();
         stdIn.nextLine();
-        int[] digits = new int[numDigits];
+        int[] numbers = new int[numNumbers];
 
-        for (int i = 0; i < digits.length; i++) {
-            System.out.print("Enter digit for position " + (i + 1) + " > ");
-            digits[i] = stdIn.nextInt() % 10;
+        for (int i = 0; i < numbers.length; i++) {
+            System.out.print("Enter number for position " + (i + 1) + " > ");
+            numbers[i] = stdIn.nextInt();
             stdIn.nextLine();
         }
-        return digits;
+        return numbers;
     }
 
-    private static GameSolution calcGameSolutions (int i, int j, int[] digits,
+    private static GameSolution calcGameSolutions (int i, int j, int[] numbers,
             MemoMatrix<GameSolution> memo) {
-        if (i < 0 || j < 0 || i >= digits.length || j >= digits.length) {
+        if (i < 0 || j < 0 || i >= numbers.length || j >= numbers.length) {
             return new GameSolution(0, 0, false);
         }
         if (i < j) {
@@ -50,11 +50,11 @@ public class Main {
             return memo.recall(i, j);
         }
 
-        GameSolution solFirst = calcGameSolutions(i, j + 1, digits, memo);
-        int firstTotal = solFirst.previousTotal + digits[j];
+        GameSolution solFirst = calcGameSolutions(i, j + 1, numbers, memo);
+        int firstTotal = solFirst.previousTotal + numbers[j];
 
-        GameSolution solLast = calcGameSolutions(i - 1, j, digits, memo);
-        int lastTotal = solLast.previousTotal + digits[i];
+        GameSolution solLast = calcGameSolutions(i - 1, j, numbers, memo);
+        int lastTotal = solLast.previousTotal + numbers[i];
 
         GameSolution sol;
         if (firstTotal >= lastTotal) {
@@ -66,20 +66,22 @@ public class Main {
         return sol;
     }
 
-    private static void playGame (int[] digits, MemoMatrix<GameSolution> memo) {
-        int i = digits.length - 1, // Back pointer
+    private static void playGame (int[] numbers, MemoMatrix<GameSolution> memo) {
+        int i = numbers.length - 1, // Back pointer
                 j = 0, // Front pointer
                 round = 1,
                 sumPlayer = 0,
-                sumComputer = 0;
+                sumComputer = 0,
+                maxNumLen = Integer.toString(Arrays.stream(numbers).max()
+                        .orElse(Integer.MIN_VALUE)).length();
 
         while (i >= j) {
             System.out.printf("Round %d: ", round);
-            for (int k = 0; k < digits.length; k++) {
+            for (int k = 0; k < numbers.length; k++) {
                 if (k < j || k > i) {
-                    System.out.print("  ");
+                    System.out.printf("%"+maxNumLen+"s ", " ");
                 } else {
-                    System.out.print(digits[k] + " ");
+                    System.out.printf("%"+maxNumLen+"s ", numbers[k]);
                 }
             }
             System.out.printf(" Player: %d, Computer: %d%n", sumPlayer,
@@ -90,27 +92,31 @@ public class Main {
                 GameSolution sol = memo.recall(i, j);
                 if (sol.fromFirst) {
                     System.out.println("Computer chooses First");
-                    sumComputer += digits[j];
+                    sumComputer += numbers[j];
                     j++;
                 } else {
                     System.out.println("Computer chooses Last");
-                    sumComputer += digits[i];
+                    sumComputer += numbers[i];
                     i--;
                 }
             } else {
                 // Player Turn
                 System.out.printf("Take first (%d) or last (%d)? > ",
-                        digits[j], digits[i]);
-                char choice = stdIn.nextLine().charAt(0);
+                        numbers[j], numbers[i]);
+                String choiceStr = stdIn.nextLine();
+                boolean choseFront = false;
+                choseFront = choiceStr.equalsIgnoreCase("first") ||
+                             choiceStr.equalsIgnoreCase("f") ||
+                             (choiceStr.matches("-?\\d+") &&
+                                 Integer.parseInt(choiceStr) == numbers[j]);
 
-                if (choice == 'f' || choice == 'F' || choice - '0' ==
-                        digits[j]) {
+                if (choseFront) {
                     System.out.println("Player chooses First");
-                    sumPlayer += digits[j];
+                    sumPlayer += numbers[j];
                     j++;
                 } else {
                     System.out.println("Player chooses Last");
-                    sumPlayer += digits[i];
+                    sumPlayer += numbers[i];
                     i--;
                 }
             }
